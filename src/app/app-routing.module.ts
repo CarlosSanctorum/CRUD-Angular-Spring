0666d32dcfafee1936284be085a146f91d57a90f
Carlos Santos <carlos.santosgon77@gmail.com>
2024-05-08 23:52:58 -0300

    Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
+//748
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
