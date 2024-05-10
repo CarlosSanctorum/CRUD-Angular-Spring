f7f910a638dd15f4cb69bc46ba461b19a072ed84
Carlos Santos <carlos.santosgon77@gmail.com>
2024-05-10 17:15:05 -0300

    Resolvendo problemas

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+
+})
+export class AppComponent {
+  title = 'projeto2.1';
+}
